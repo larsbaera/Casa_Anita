5b67a5c11ace516bfc6bce41b4b1100309c59667
larsbaera <lars-baera@gmail.com>
2019-11-16 03:01:17 +0100

    Update 15/16h november, fixed a lot of minor fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,39 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   var elems = document.querySelectorAll(".sidenav");
-//   var instances = M.Sidenav.init(elems, options);
-// });
-
-// $(document).ready(function() {
-//   $(".sidenav").sidenav();
-// });
-$(document).ready(function() {
-  $(".scrollspy").scrollSpy();
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".scrollspy");
+  var options = {
+    throttle: 10000,
+    scrollOffset: 50
+  };
+  var instances = M.ScrollSpy.init(elems, options);
 });
-$(document).ready(function() {
-  $(".materialboxed").materialbox();
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".materialboxed");
+  var options = { inDuration: 500, outDuration: 500 };
+  var instances = M.Materialbox.init(elems, options);
 });
 
 document.addEventListener("DOMContentLoaded", function() {
   var elems = document.querySelectorAll(".sidenav");
+  var options = {
+    coverTrigger: false,
+    inDuration: 300,
+    outDuration: 300
+  };
   var instances = M.Sidenav.init(elems, options);
 });
-$(document).ready(function() {
-  $(".sidenav").sidenav();
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".dropdown-trigger");
+  var options = { hover: true, alignment: "right", coverTrigger: false };
+  var instances = M.Dropdown.init(elems, options);
 });
-$(".dropdown-trigger").dropdown();
+// document.addEventListener("DOMContentLoaded", function() {
+//   var elems = document.querySelectorAll(".carousel");
+//   var options = { fullWidth: true, indicators: true };
+//   var instances = M.Carousel.init(elems, options);
+// });
+$(".carousel.carousel-slider")
+  .carousel({
+    indicators: true
+  })
+  .height(700);
